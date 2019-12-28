6700e6ed2e91c008afe5e39d65ed08d31b44a14b
Apokochito <dpinto1@ucol.mx>
2019-12-27 23:01:12 -0600

    Triangles and exceptions

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,7 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+   pass 
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
